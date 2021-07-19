ca09e7037e62c22b253bc2da9fb08cc1d78e5da3
Karim Abdo <karim.abdo.1991@gmail.com>
2021-07-19 12:14:25 +0200

    Supporting null safety

--- lib/swipe_up.dart
+++ lib/swipe_up.dart
@@ -5,15 +5,15 @@ import 'package:flutter/material.dart';
 class SwipeUp extends StatefulWidget {
   final Widget body;
   final Widget child;
-  final VoidCallback onSwipe;
+  final VoidCallback? onSwipe;
   final double sensitivity;
   final bool showArrow;
   final Color color;
   final bool animate;
   final bool expand;
   SwipeUp(
-      {@required this.body,
-        @required this.child,
+      {required this.body,
+        required this.child,
         this.onSwipe,
         this.sensitivity = 0.5,
         this.showArrow = true,
@@ -28,11 +28,11 @@ class SwipeUp extends StatefulWidget {
 }
 
 class _SwipeUpState extends State<SwipeUp> with SingleTickerProviderStateMixin {
-  double _initialOffset;
-  double _swipeOffset;
+  late double _initialOffset;
+  late double _swipeOffset;
 
-  AnimationController _animationController;
-  CurvedAnimation _animation;
+  late AnimationController _animationController;
+  late CurvedAnimation _animation;
 
   @override
   void initState() {
@@ -79,7 +79,7 @@ class _SwipeUpState extends State<SwipeUp> with SingleTickerProviderStateMixin {
       },
       onVerticalDragEnd: (details) {
         if (_swipeOffset > MediaQuery.of(context).size.height / 8) {
-          widget.onSwipe();
+          widget.onSwipe!();
         }
         this.setState(() {
           _swipeOffset = 0.0;
